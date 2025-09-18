e5585b37f7649fd412c713ecf32cade99fa88d98
thpir <92860191+thpir@users.noreply.github.com>
2025-09-18 11:15:49 +0200

    added general house analysis

---
+++ solar_predictor/main.py
@@ -0,0 +1,18 @@
+# acquire through visual crossing API
+temperature = None
+cloudcover = None
+solarenergy = None
+uvindex = None
+
+# acquire through sun calculator
+solar_azimuth = None
+solar_elevation = None
+
+# acquire through calculation
+day_of_year_sine = None
+day_of_year_cosine = None
+
+# import model
+# import scaler
+# make prediction
+# send prediction to influxdb
